54d0c16dbc0ce3bac0ede578c297bbfbb45b0e8c
Marieerin <m.oshaughnessy@outlook.com>
2021-05-11 14:23:04 -0400

    Added to styling including background, and font.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import './App.css';
 import React, { useState } from 'react';
 
 function DraggingShapes() {
-  const [shape, setShape] = useState('')
+  const [shape, setShape] = useState('');
+  const [buttonVisibility, setButtonVisibility] = useState('');
 
-  let shapes = ['square', 'circle', 'triangle']
+  let isDragging = false;
+  let x = 0;
+  let y = 0;
+  let shapes = ['square', 'circle', 'triangle'];
+  // let buttonVisitbility = ''
+
 
   let start = () => {
-    setShape(shapes[Math.floor(Math.random() * shapes.length)])
+    setShape(shapes[Math.floor(Math.random() * shapes.length)]);
+    setButtonVisibility('hidden');
   }
-  // console.log(shape);
+
+  // drag function
+  // follow mouse
+  // check if dragged shape 
 
   return (
     <div className="App">
-      {/*   SHAPE PORT -------------------------------------- */}
-      <div className="shapePort">
-        <div className="square"></div>
-        <div className="triangle"></div>
-        <div className="circle"></div>
-      </div>
-      {/*   DRAG PORT -------------------------------------- */}
-      <div className="dragPort">
-        <button onClick={start}>start</button>
-        <div className={shape}></div>
+      <h1>Drag and Drop! Match the Shapes!</h1>
+      <div id='canvas'>
+        {/*   SHAPE PORT -------------------------------------- */}
+        <div className="shapePort">
+          <div className="square"></div>
+          <div className="triangle"></div>
+          <div className="circle"></div>
+        </div>
+        {/*   DRAG PORT -------------------------------------- */}
+        <div className="dragPort">
+          <button onClick={start} style={{visibility: `${buttonVisibility}`}}>start</button>
+          <div className={shape} onClick={start}></div>
+        </div>
       </div>
     </div>
   );

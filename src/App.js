a8a08a586f4e95512b1f310e31551ca6db322251
Marieerin <m.oshaughnessy@outlook.com>
2021-05-11 16:29:42 -0400

    added reset button and portrait media query

--- src/App.js
+++ src/App.js
@@ -8,18 +8,23 @@ function DraggingShapes() {
   let isDragging = false;
   let x = 0;
   let y = 0;
-  let shapes = ['square', 'circle', 'triangle'];
-  // let buttonVisitbility = ''
+  let shapes = ['square', 'circle', 'triangle', 'pentagon'];
 
-
+  // START GAME FUNCTION
+    // 
   let start = () => {
     setShape(shapes[Math.floor(Math.random() * shapes.length)]);
-    setButtonVisibility('hidden');
+    setButtonVisibility('none');
   }
 
   // drag function
   // follow mouse
   // check if dragged shape 
+    // check x & y axis between a 150px radius 
+    // if same class name is same call start function 
+      // correct function (not made yet) call that too
+    // if class name isnt same then return to starting position 
+      // call tryAgain function (not made yet)
 
   return (
     <div className="App">
@@ -30,13 +35,23 @@ function DraggingShapes() {
           <div className="square"></div>
           <div className="triangle"></div>
           <div className="circle"></div>
+          <div className="pentagon"></div>
         </div>
         {/*   DRAG PORT -------------------------------------- */}
         <div className="dragPort">
-          <button onClick={start} style={{visibility: `${buttonVisibility}`}}>start</button>
+          <button 
+          onClick={start} 
+          style={{display: `${buttonVisibility}`}}
+          id="startButton"
+          >
+            start
+          </button>
           <div className={shape} onClick={start}></div>
         </div>
       </div>
+      <button id="restartButton"> 
+        restart
+      </button>
     </div>
   );
 }

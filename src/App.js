f470673406d9c70ba4c29a4a8091bac3b5cd4c1d
Marieerin <m.oshaughnessy@outlook.com>
2021-05-12 11:14:35 -0400

    began functionality for drag and drop and organized stylesheet

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import React, { useState } from 'react';
 function DraggingShapes() {
   const [shape, setShape] = useState('');
   const [buttonVisibility, setButtonVisibility] = useState('');
+  const [level, setLevel] = useState('');
+  let shapes = ['square', 'circle', 'triangle', 'pentagon'];
+  let levels = ['Level 1', 'Level 2', 'Level 3'];
 
+  // When shape is clicked isDragging becomes true
   let isDragging = false;
   let x = 0;
   let y = 0;
-  let shapes = ['square', 'circle', 'triangle', 'pentagon'];
+
+  let draggingCanvas = document.getElementById('canvas');
 
   // START GAME FUNCTION
-    // 
   let start = () => {
     setShape(shapes[Math.floor(Math.random() * shapes.length)]);
     setButtonVisibility('none');
-  }
+    setLevel(levels[0]);
+  };
 
-  // drag function
+  // RESTART GAME FUNCTION
+  let restart = () => {
+    setShape('');
+    setButtonVisibility('');
+  };
+
+  // drag function (may be broken down into many functions)
   // follow mouse
   // check if dragged shape 
     // check x & y axis between a 150px radius 
@@ -32,13 +43,16 @@ function DraggingShapes() {
       <div id='canvas'>
         {/*   SHAPE PORT -------------------------------------- */}
         <div className="shapePort">
-          <div className="square"></div>
-          <div className="triangle"></div>
-          <div className="circle"></div>
-          <div className="pentagon"></div>
+          <div className="square draggable" draggable='true'></div>
+          <div className="triangle draggable" draggable='true'></div>
+          <div className="circle draggable" draggable='true'></div>
+          <div className="pentagon draggable" draggable='true'></div>
         </div>
         {/*   DRAG PORT -------------------------------------- */}
         <div className="dragPort">
+          <span id="level">
+            {level}
+          </span>
           <button 
           onClick={start} 
           style={{display: `${buttonVisibility}`}}
@@ -46,10 +60,11 @@ function DraggingShapes() {
           >
             start
           </button>
+          {/* on hover might trigger shapecheck */}
           <div className={shape} onClick={start}></div>
         </div>
       </div>
-      <button id="restartButton"> 
+      <button id="restartButton" onClick={restart}> 
         restart
       </button>
     </div>
